f7278432fcc3cd8902b0d961805ecd59980c7c0e
Lea Guzmicka <lea.guzmicka@outlook.com>
2025-06-21 22:52:25 +0300

    Add grow functionality to on ball click

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var initSize = 100;
+
 function onBallClick() {
-  console.log('You clickeds');
+  var elBall = document.getElementsByClassName('ball')[0];
+  initSize += 50;
+  elBall.style.height = initSize + 'px';
+  elBall.style.width = initSize + 'px';
+  elBall.innerHTML = initSize;
 }
