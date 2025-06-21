6cf3c33b619b9ad7b78cdc5508fa3f2d1a0c52b8
Lea Guzmicka <lea.guzmicka@outlook.com>
2025-06-21 23:05:01 +0300

    Add grow limitation

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var initSize = 100;
 function onBallClick() {
   var elBall = document.getElementsByClassName('ball')[0];
   initSize += 50;
+  if (initSize > 400) initSize = 100;
   elBall.style.height = initSize + 'px';
   elBall.style.width = initSize + 'px';
   elBall.innerHTML = initSize;
