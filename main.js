dbd0f6f2bbee063c22f95a05fc89152cddbb459b
Lea Guzmicka <lea.guzmicka@outlook.com>
2025-06-24 19:47:31 +0300

    Connect util to index and use function GetRandomInt at main

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var initSize = 100;
 
 function onBallClick() {
   var elBall = document.getElementsByClassName('ball')[0];
-  initSize += 50;
+  initSize += getRandomInt(20, 60);
   if (initSize > 400) initSize = 100;
   elBall.style.height = initSize + 'px';
   elBall.style.width = initSize + 'px';
